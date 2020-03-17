1ae4a4ffa269413bc384ea4adda4c56fe9412c5c
JaysonD <jaduff20@gmail.com>
2020-03-17 18:05:34 -0400

    main forecast date is now functional

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,65 +1,66 @@
-// build a weather dashboard that will run in the browser and
-//     feature dynamically updated HTML and CSS.
-//     Use `localStorage` to store any persistent data.
 
-// ```
-// AS A traveler
-// I WANT to see the weather outlook for multiple cities
-// SO THAT I can plan a trip accordingly
 // ```
 //=========================================================================================
 // API KEY: 78ebcf5b45dec15369fc1101ca8eeb5d
 // example call: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=78ebcf5b45dec15369fc1101ca8eeb5d
 //=========================================================================================
 // ```
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city//
-
-$("#submit-city").on("click", function(event) {
-  event.preventDefault();
-  var cityName = $("#city-search").val();
-  var cityButton = $("<li>").text(cityName);
-  cityButton.addClass("list-group-item");
-  $("#city-list").prepend(cityButton);
-  $("input[type='text']").val(""); //to reset input field (this may cause error down the line, check back later)
-  cityButton.on("click", function() {
-    var apikey = "78ebcf5b45dec15369fc1101ca8eeb5d";
-    var queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      cityName +
-      "&appid=" +
-      apikey +
-      "&units=imperial";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      $(".main-city-name").html("<h2>" + response.name + " - " + "</h2>");
-      //https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
-      var iconNumber = response.weather[0].icon;
-      var iconURL = "http://openweathermap.org/img/w/" + iconNumber + ".png";
-      $("#wicon").removeClass("collapse");
-      $("#wicon").attr("src", iconURL);
-      $(".description").html(
-        "<h6>Currently: " + response.weather[0].description + "</h6>"
-      );
-      $(".description").addClass("text-muted");
-      $(".temp").text("Temperature: " + response.main.temp + "F");
-      $(".humidity").text("Humidity: " + response.main.humidity);
-      $(".wind").text("Wind Speed: " + response.wind.speed);
-      var queryUV =
-        "https://api.openweathermap.org/data/2.5/uvi?lat=" +
-        response.coord.lat +
-        "&lon=" +
-        response.coord.lon +
+$(document).ready(function() {
+  $("#submit-city").on("click", function(event) {
+    event.preventDefault();
+    var cityName = $("#city-search").val();
+    var cityButton = $("<li>").text(cityName);
+    cityButton.addClass("list-group-item btn-light");
+    $("#city-list").prepend(cityButton);
+    $("input[type='text']").val(""); //to reset input field (this may cause error down the line, check back later)
+    cityButton.on("click", function() {
+      var apikey = "78ebcf5b45dec15369fc1101ca8eeb5d";
+      var queryURL =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityName +
         "&appid=" +
-        apikey;
-      console.log(queryUV);
+        apikey +
+        "&units=imperial";
+        //==========CURRENT FORECAST CALL============//
       $.ajax({
-        url: queryUV,
+        url: queryURL,
         method: "GET"
       }).then(function(response) {
-        $(".uvindex").text("UV Index = " + response.value);
+        //https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+        var currentDate = new Date(response.dt * 1000).toLocaleDateString("en-US")
+        $("#city-forecast").addClass("border border-dark rounded");
+        $(".main-city-name").html("<h2>" + response.name + " - " + currentDate + "</h2>");
+        //https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+        var iconNumber = response.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + iconNumber + ".png";
+        $("#wicon").removeClass("collapse");
+        $("#wicon").attr("src", iconURL);
+        $(".description").html(
+          "<h6>Currently: " + response.weather[0].description + "</h6>"
+        );
+        $(".description").addClass("text-muted");
+        $(".temp").text("Temperature: " + response.main.temp + "F");
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        console.log(response)
+        var queryUV =
+          "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+          response.coord.lat +
+          "&lon=" +
+          response.coord.lon +
+          "&appid=" +
+          apikey;
+        //=========UV INDEX CALL==================//
+        $.ajax({
+          url: queryUV,
+          method: "GET"
+        }).then(function(response) {
+          $(".uvindex").text("UV Index = " + response.value);
+        });
+        //===========FIVE DAY FORECAST CALL ==============//
+        $.ajax({
+
+        })
       });
     });
   });
